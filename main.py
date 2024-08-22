1ad9316f8ebe61043b7c6f69c1c5d28597d924c7
JaimeNarvaez <jaimeangel936@gmail.com>
2024-08-22 16:01:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
 if word in meme_dict.keys():
     # ¿Qué debemos hacer si se encuentra la palabra?
 else:
-    # ¿Qué hacer si no se encuentra la palabra?
+    # ¿Qué hacer si no se encuentra la palabra? esto seria un cambion dentro del codigo 
 
 #esto es un cambio
